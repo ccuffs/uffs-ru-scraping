d6cfb2774144667210e5ebf09fcc5b7490aec696
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2022-09-09 11:00:24 -0300

    fix: solve some bugs

--- tests/test.php
+++ tests/test.php
@@ -5,4 +5,6 @@ require __DIR__ . '/../vendor/autoload.php';
 use CCUFFS\Scrap\UniversityRestaurantUFFS;
 
 $ur = new UniversityRestaurantUFFS();
-print_r($ur->getMenuByCampus($ur->campus["passo-fundo"]));
+
+$list = $ur->getMenuByCampus($ur->campus["realeza"]);
+print_r($list);
